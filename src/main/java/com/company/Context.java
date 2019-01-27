f422c2e4a62d666ef6ed60c3880e67b58ec4b86b
tural <turalmz@gmail.com>
2019-01-27 21:14:21 +0400

    Context was not properly committed , so i commit it again

---
+++ src/main/java/com/company/Context.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company;
+
+import com.company.dao.impl.CountryDaoImpl;
+import com.company.dao.impl.JobHistoryDaoImpl;
+import com.company.dao.impl.DepartmentDaoImpl;
+import com.company.dao.impl.EmployeeDaoImpl;
+import com.company.dao.impl.JobDaoImpl;
+import com.company.dao.impl.LocationDaoImpl;
+import com.company.dao.impl.RegionDaoImpl;
+import com.company.dao.inter.CountryDaoInter;
+import com.company.dao.inter.DepartmentDaoInter;
+import com.company.dao.inter.LocationDaoInter;
+import com.company.dao.inter.EmployeeDaoInter;
+import com.company.dao.inter.JobHistoryDaoInter;
+import com.company.dao.inter.RegionDaoInter;
+import com.company.dao.inter.JobDaoInter;
+
+/**
+ *
+ * @author TURAL
+ */
+public class Context {
+
+    public static EmployeeDaoInter instanceEmployeeDao() {
+        return new EmployeeDaoImpl();
+    }
+
+    public static DepartmentDaoInter instanceDepartmentDao() {
+        return new DepartmentDaoImpl();
+    }
+
+    public static JobDaoInter instanceJobDao() {
+        return new JobDaoImpl();
+    }
+
+    public static LocationDaoInter instanceLocationDao() {
+        return new LocationDaoImpl();
+    }
+
+    public static JobHistoryDaoInter instanceJobHistoryDao() {
+        return new JobHistoryDaoImpl();
+    }
+
+    public static RegionDaoInter instanceRegionDao() {
+        return new RegionDaoImpl();
+    }
+
+    public static CountryDaoInter instanceCountryDao() {
+        return new CountryDaoImpl();
+    }
+}
